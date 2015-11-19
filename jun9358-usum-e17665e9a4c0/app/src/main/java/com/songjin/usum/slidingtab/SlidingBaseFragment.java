0c20533a87b72eb2a73340e6362d5f202aaf3769
WindArrow <wind.dong.dream@gmail.com>
2015-11-19 12:19:58 +0900

    Revert "처음"
    This reverts commit 8d9f44502821f93fa6da448a67b830a17d525039.
    
    Conflicts:
    
    jun9358-usum-e17665e9a4c0/app/src/main/java/com/songjin/usum/controllers
    /activities/AddProductsActivity.java

--- jun9358-usum-e17665e9a4c0/app/src/main/java/com/songjin/usum/slidingtab/SlidingBaseFragment.java
+++
@@ -1,7 +0,0 @@
-package com.songjin.usum.slidingtab;
-
-import android.support.v4.app.Fragment;
-
-public abstract class SlidingBaseFragment extends Fragment {
-    public abstract void onPageSelected();
-}
